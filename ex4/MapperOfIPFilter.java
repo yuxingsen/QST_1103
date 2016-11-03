c107dcb712bd4f13d661650be91b84200ef266a5
yuxingsen <15625583996@163.com>
2016-11-03 15:42:31 +0800

    Update MapperOfIPFilter.java

--- ex4/MapperOfIPFilter.java
+++ ex4/MapperOfIPFilter.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class MapperOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		String pattern="(\\d+\\.\\d+\\.\\d+\\.\\d+).{5}\\[(\\d+\\/[a-zA-Z]+\\/\\d+\\:\\d+\\:\\d+\\:\\d+).{13}\\/((show|musicians).\\d+)";  
+		Pattern r = Pattern.compile(pattern);
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
+			Matcher m = r.matcher(line);
+			if (m.find()){
+				System.out.println(m.group(1)+"\t"+m.group(2));//把ip与时间输出
+			}
 		}
 	}
 }
